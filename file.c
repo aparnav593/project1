59b8a081ac2f6229642451ddd6170061667f757d
aparnav593 <166357189+aparnav593@users.noreply.github.com>
2026-02-11 21:40:39 +0530

    Update file.c

--- file.c
+++ file.c
@@ -1,3 +1,28 @@
-#include<stdio.h>
-int main()
-printf(" who are uuuu?");
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter the elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int max = arr[0];  
+
+    for(i = 1; i < n; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    printf("The largest number in the array is: %d\n", max);
+
+    return 0;
+}
+
